511f02b136fb696fedfa71f0834af82ea8f2a0de
yizhakg <yizhakg.gmail.com>
2021-03-01 00:43:22 +0200

    update

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function toggleMenu() {
+  if (window.innerWidth < 600) {
+    let navUl = document.querySelector(".navUl");
+    let toggle = document.querySelector(".toggle");
+    navUl.classList.toggle("active");
+    toggle.classList.toggle("active");
+  }
+}
+
 function toggleJob(e) {
   const job = document.querySelector(".currentJob");
   document.getElementById("currentJobTitle").innerHTML = e.innerHTML.slice(31);
@@ -11,16 +20,24 @@ function changeInfoClass(num) {
   jobsNavChildrens[num].classList.add("active");
 }
 function navToInfo(num) {
-  switch(num){
+  switch (num) {
     case 0:
       jobsInfo.scrollTop = info1.offsetTop - 66;
-      break
+      break;
     case 1:
       jobsInfo.scrollTop = info2.offsetTop - 66;
-      break
+      break;
     case 2:
       jobsInfo.scrollTop = info3.offsetTop - 66;
-      break
+      break;
+    case 3:
+      {
+        applying.scrollIntoView();
+        setTimeout(() => {
+          changeInfoClass(3);
+        }, 900);
+      }
+      break;
   }
 }
 
@@ -28,6 +45,7 @@ const jobsInfo = document.getElementById("jobsInfo");
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
+const applying = document.getElementById("applying");
 jobsInfo.addEventListener("scroll", (e) => {
   jobsInfo.scrollTop >= 0 &&
     jobsInfo.scrollTop < info2.offsetTop - 68 &&
